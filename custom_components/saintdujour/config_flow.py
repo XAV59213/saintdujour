ea91f5443cc12ecd7da73bbe360b394d3965319e
XAV59213 <153436936+XAV59213@users.noreply.github.com>
2025-06-05 17:09:40 +0200

    Update config_flow.py

--- custom_components/saintdujour/config_flow.py
+++ custom_components/saintdujour/config_flow.py
@@ -1,6 +1,9 @@
 """Flux de configuration pour Saints du Jour."""
+import logging
 from homeassistant import config_entries
 from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Gérer le flux de configuration."""
@@ -9,6 +12,15 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         """Gérer l'étape initiale."""
+        _LOGGER.debug("Démarrage du flux de configuration pour %s", DOMAIN)
         if self._async_current_entries():
+            _LOGGER.debug("Entrée existante trouvée, annulation")
             return self.async_abort(reason="single_instance_only")
+        
+        _LOGGER.debug("Création d'une nouvelle entrée de configuration")
         return self.async_create_entry(title="Saints du Jour", data={})
+
+    async def async_step_import(self, import_data=None):
+        """Gérer l'importation automatique."""
+        _LOGGER.debug("Importation automatique pour %s", DOMAIN)
+        return await self.async_step_user(None)
