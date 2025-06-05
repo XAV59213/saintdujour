e66b17cba966b44365c2667483ec10053903c9db
XAV59213 <153436936+XAV59213@users.noreply.github.com>
2025-06-05 17:18:31 +0200

    Update config_flow.py

--- custom_components/saintdujour/config_flow.py
+++ custom_components/saintdujour/config_flow.py
@@ -1,6 +1,8 @@
 """Flux de configuration pour Saints du Jour."""
 import logging
+import voluptuous as vol
 from homeassistant import config_entries
+from homeassistant.core import callback
 from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
@@ -11,16 +13,32 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        """Gérer l'étape initiale."""
-        _LOGGER.debug("Démarrage du flux de configuration pour %s", DOMAIN)
+        """Gérer l'étape utilisateur."""
+        _LOGGER.info("Démarrage du flux de configuration utilisateur pour %s", DOMAIN)
+
         if self._async_current_entries():
-            _LOGGER.debug("Entrée existante trouvée, annulation")
+            _LOGGER.warning("Entrée existante trouvée, annulation du flux")
             return self.async_abort(reason="single_instance_only")
-        
-        _LOGGER.debug("Création d'une nouvelle entrée de configuration")
-        return self.async_create_entry(title="Saints du Jour", data={})
+
+        if user_input is not None:
+            _LOGGER.info("Utilisateur a confirmé l'ajout de l'intégration")
+            return self.async_create_entry(title="Saints du Jour", data={})
+
+        # Afficher un formulaire demandant si l'utilisateur veut ajouter l'intégration
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({}),
+            description_placeholders={
+                "description": "Voulez-vous ajouter l'intégration Saints du Jour pour afficher le saint du jour ?"
+            }
+        )
 
     async def async_step_import(self, import_data=None):
         """Gérer l'importation automatique."""
-        _LOGGER.debug("Importation automatique pour %s", DOMAIN)
-        return await self.async_step_user(None)
+        _LOGGER.info("Exécution du flux d'importation automatique pour %s", DOMAIN)
+        if self._async_current_entries():
+            _LOGGER.warning("Entrée existante trouvée, annulation de l'importation")
+            return self.async_abort(reason="single_instance_only")
+        
+        _LOGGER.info("Création automatique de l'entrée de configuration")
+        return self.async_create_entry(title="Saints du Jour", data={})
